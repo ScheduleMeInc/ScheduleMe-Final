6b147620a564421c5db05b090e878a10ceb6da8d
Dave Hamilton <orders@davethenerd.com>
2014-03-19 00:28:26 -0400

    just correcting the index

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@
         echo "<title>PHP/Git Test</title>";
         echo "</head>";
         echo "<body>";
-            echo "<h2>Hello World-dev!!!!</h2>";
+            echo "<h2>Hello World-Final!!!!</h2>";
             echo "Another line was added 20140304.<br />";
             echo "Yet, another line was added 20140304.<br />";
         echo "</body>";
