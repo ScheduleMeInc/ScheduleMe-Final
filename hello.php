43ddf02ec88955bedad30962749628b38ab1305a
Dave Hamilton <orders@davethenerd.com>
2014-03-18 23:43:18 -0400

    Uploading hello.php

---
+++ hello.php
@@ -0,0 +1,12 @@
+<?php
+    echo "<html>";
+        echo "<head>";
+        echo "<title>PHP/Git Test</title>";
+        echo "</head>";
+        echo "<body>";
+            echo "<h2>Hello World-dev!!!!</h2>";
+            echo "Another line was added 20140304.<br />";
+            echo "Yet, another line was added 20140304.<br />";
+        echo "</body>";
+    echo "</html>";
+?>
